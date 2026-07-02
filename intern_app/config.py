8d6d9938b7fba63f0b8cdf90687c7ba69af7c97c
unknown <sudhirmishra885@gmail.com>
2026-07-02 13:05:42 +0530

    Configure SQLAlchemy database

---
+++ intern_app/config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+print(load_dotenv)
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
+)
+
+DATABASE_URL = os.getenv("DATABASE_URL")
